3183e587f799f83851f4f3f143d10caa92415dc1
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-08 17:42:09 +0300

    complete timer

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -1,16 +1,40 @@
 import BSN from 'bootstrap.native';
-// const PROMPT_DELAY = 1000;
-// const MAX_PROMP_ATTEENTIONS = 3;
 
-// let promptCounter = 0;
-// let hasSubscribed = false;
+const myModalInstance = new BSN.Modal('#myModal');
 
-// const intervalId = setInterval(() => {
-//   if (promptCounter === MAX_PROMP_ATTEENTIONS) {
-//     console.log('Нужно остановить интервал');
-//     clearInterval(intervalId);
-//     return;
-//   }
-//   console.log('Подпишись на рассылку!');
-//   promptCounter += 1;
-// }, PROMPT_DELAY);
+const refs = {
+  modal: document.querySelector('#myModal'),
+  btnSubcribe: document.querySelector('button[data-subscribe]'),
+};
+
+const PROMPT_DELAY = 1000;
+const MAX_PROMP_ATTEENTIONS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+
+openModal();
+
+refs.modal.addEventListener('hide.bs.modal', () => {
+  openModal();
+});
+refs.btnSubcribe.addEventListener('click', () => {
+  onSubscribeBtnClick();
+});
+
+function openModal() {
+  if (promptCounter === MAX_PROMP_ATTEENTIONS || hasSubscribed) {
+    console.log('Максимальное кол-во надоеданий или юзер подписался');
+    return;
+  }
+  setTimeout(() => {
+    console.log('Открываем модалку!');
+    promptCounter += 1;
+    myModalInstance.show();
+  }, PROMPT_DELAY);
+}
+
+function onSubscribeBtnClick() {
+  hasSubscribed = true;
+
+  myModalInstance.hide();
+}
